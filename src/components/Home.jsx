4749d72f856dd3a262050a7936da1653a6daf767
marianosouGuil <mariano.soule@guil.cl>
2022-07-20 17:49:44 -0300

    some changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
     <div className="home-container">
       <h5>So, you want to travel to</h5>
       <h1>Space</h1>
+      <h2>Prueba</h2>
       <p>
         Let’s face it; if you want to go to space, you might as well genuinely
         go to outer space and not hover kind of on the edge of it. Well sit
